08274f425cf0b50c5cdb89db4c0ca72a908d53b5
Bruno <bruno_marin@hotmail.com>
2019-06-06 16:44:50 -0300

    calendar and flexbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_171806) do
+ActiveRecord::Schema.define(version: 2019_06_05_175425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2019_06_05_171806) do
     t.string "photo"
     t.float "latitude"
     t.float "longitude"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
