b47764334b09c319841b9862bc1a24376a33f032
Daniel Belarmino <belarba@gmail.com>
2019-06-06 09:16:08 -0300

    some improvments

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,10 +2,11 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
 
-     @markers = @cars.map do |car|
+    @markers = @cars.map do |car|
       {
         lat: car.latitude,
         lng: car.longitude
+        # infoWindow: render_to_string(partial: "infowindow", locals: { car: car })
       }
     end
   end
@@ -40,7 +41,10 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
     @user = current_user
-    @markers = [lat: @car.latitude, lng: @car.longitude]
+    @markers = [lat: @car.latitude,
+                lng: @car.longitude
+                # infoWindow: render_to_string(partial: "infowindow", locals: { car: @car })
+               ]
   end
 
   def destroy
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
-
 initMapbox();
+import { initAutocomplete } from '../plugins/init_autocomplete';
+initAutocomplete();
