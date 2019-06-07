4ef89d42d515967010a1713b7e2e6184c77f3589
Felipe Rocha <felipewp200@gmail.com>
2019-06-07 12:03:25 -0300

    ajustes

--- app/javascript/plugins/calculate_total.js
+++ app/javascript/plugins/calculate_total.js
@@ -1,7 +1,9 @@
 const calculate = () => {
   const calculate_total = () => {
-    const date1 = new Date(`${document.getElementById('booking_start_date_2i').value}/${document.getElementById('booking_start_date_3i').value}/${document.getElementById('booking_start_date_1i').value}`);
-    const date2 = new Date(`${document.getElementById('booking_end_date_2i').value}/${document.getElementById('booking_end_date_3i').value}/${document.getElementById('booking_end_date_1i').value}`);
+    const date1 = new Date(document.getElementById('booking_start_date').value);
+    const date2 = new Date(document.getElementById('booking_end_date').value);
+    console.log(date1);
+    console.log(date2);
     if (date1 || date2) {
       let diffDays = Math.round((date2-date1)/(1000*60*60*24));
       if (diffDays < 1) { diffDays = 1 };
@@ -12,12 +14,9 @@ const calculate = () => {
   };
 
   document.getElementById('booking_total_amount').value = document.getElementById('daily_amount').value;
-  document.getElementById("booking_start_date_3i").addEventListener("click", calculate_total);
-  document.getElementById("booking_end_date_3i").addEventListener("click", calculate_total);
-  document.getElementById("booking_start_date_2i").addEventListener("click", calculate_total);
-  document.getElementById("booking_end_date_2i").addEventListener("click", calculate_total);
-  document.getElementById("booking_start_date_1i").addEventListener("click", calculate_total);
-  document.getElementById("booking_end_date_1i").addEventListener("click", calculate_total);
+  document.getElementById("booking_start_date").addEventListener("change", calculate_total);
+  document.getElementById("booking_end_date").addEventListener("change", calculate_total);
+
 }
 export { calculate };
 
